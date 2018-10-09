522ba8424c1c05c46248cebe738e9e9e12498c48
linfengisep <wang.linfeng0213@gmail.com>
2018-10-09 23:43:29 +0200

    beginning of website

--- SpringTrainingFun/bookshop-website/src/main/java/org/bookshop/website/App.java
+++ SpringTrainingFun/bookshop-website/src/main/java/org/bookshop/website/App.java
@@ -1,10 +1,10 @@
 package org.bookshop.website;
-
 /**
  * Hello world!
  */
 public class App {
     public static void main( String[] args ){
-        System.out.println(new org.junit.Test().toString());
+        //System.out.println(new org.junit.Test().toString());
+        System.out.println("Hello Maven");
     }
 }
